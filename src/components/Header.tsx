b49eab83fba4675e38c36274d72c452ae258853e
Vitor Passos <vitorpassos16@gmail.com>
2021-04-22 19:31:50 -0300

    day#3NLW

---
+++ src/components/Header.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import colors from '../styles/colors';
+import userImg from '../assets/vitor.jpeg';
+import fonts from '../styles/fonts';
+
+export function Header() {
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.greeting}>Olá</Text>
+                <Text style={styles.userName}>Vitor</Text>
+            </View>
+            <Image source={userImg} style={styles.image} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 20,
+        marginTop: 40
+    },
+    image: {
+        width: 70,
+        height: 70,
+        borderRadius: 40,
+
+    },
+    greeting: {
+        fontSize: 32,
+        color: colors.heading,
+        fontFamily: fonts.text,
+
+    },
+    userName: {
+        fontSize: 32,
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        lineHeight: 40
+    }
+
+})
